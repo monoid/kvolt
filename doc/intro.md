5dbea403154f869c604e17be6fe8288389969eda
Ivan Boldyrev <lispnik@gmail.com>
2013-10-23 14:52:04 +0700

    Initial version from lein template.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to memcashed
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/memcashed/core_test.clj
@@ -0,0 +1,7 @@
+(ns memcashed.core-test
+  (:require [clojure.test :refer :all]
+            [memcashed.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
