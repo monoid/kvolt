10f50600ae7abfafa1f15e72c0bbbe2232a39e22
Ivan Boldyrev <lispnik+geoserver@gmail.com>
2013-11-11 22:27:47 +0700

    Compile kvolt.api AOT.
    lein run fails without it for some reason :(

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [aleph "0.3.0"]]
+  :aot [kvolt.api]
   :main kvolt.core)
